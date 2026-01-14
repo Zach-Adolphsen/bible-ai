6b25d661a41ba749d139bed5ba7496cf72167769
Zach-Adolphsen <zladolphsen@gmail.com>
2026-01-14 17:08:45 -0600

    created Next.js frontend directory and ETL directory
    ETL is used to seed the database

---
+++ etl/etl.py
@@ -0,0 +1,87 @@
+import requests
+import json
+from collections import defaultdict
+
+BASE_URL: str = "https://bible.helloao.org/api"
+TRANSLATION_ID: str = "eng_kjv"
+
+# BOOK_ABBREVIATIONS = {
+#     "Genesis": "GEN",
+#     "Exodus": "EXO",
+#     "Leviticus": "LEV",
+#     "Numbers": "NUM",
+#     "Deuteronomy": "DEU",
+#     "Joshua": "JOS",
+#     "Judges": "JDG",
+#     "Ruth": "RUT",
+#     "1 Samuel": "1SA",
+#     "2 Samuel": "2SA",
+#     "1 Kings": "1KI",
+#     "2 Kings": "2KI",
+#     "1 Chronicles": "1CH",
+#     "2 Chronicles": "2CH",
+#     "Ezra": "EZR",
+#     "Nehemiah": "NEH",
+#     "Esther": "EST",
+#     "Job": "JOB",
+#     "Psalms": "PSA",
+#     "Proverbs": "PRO",
+#     "Ecclesiastes": "ECC",
+#     "Song of Solomon": "SNG",
+#     "Isaiah": "ISA",
+#     "Jeremiah": "JER",
+#     "Lamentations": "LAM",
+#     "Ezekiel": "EZK",
+#     "Daniel": "DAN",
+#     "Hosea": "HOS",
+#     "Joel": "JOL",
+#     "Amos": "AMO",
+#     "Obadiah": "OBA",
+#     "Jonah": "JON",
+#     "Micah": "MIC",
+#     "Nahum": "NAM",
+#     "Habakkuk": "HAB",
+#     "Zephaniah": "ZEP",
+#     "Haggai": "HAG",
+#     "Zechariah": "ZEC",
+#     "Malachi": "MAL",
+#     "Matthew": "MAT",
+#     "Mark": "MRK",
+#     "Luke": "LUK",
+#     "John": "JHN",
+#     "Acts": "ACT",
+#     "Romans": "ROM",
+#     "1 Corinthians": "1CO",
+#     "2 Corinthians": "2CO",
+#     "Galatians": "GAL",
+#     "Ephesians": "EPH",
+#     "Philippians": "PHP",
+#     "Colossians": "COL",
+#     "1 Thessalonians": "1TH",
+#     "2 Thessalonians": "2TH",
+#     "1 Timothy": "1TI",
+#     "2 Timothy": "2TI",
+#     "Titus": "TIT",
+#     "Philemon": "PHM",
+#     "Hebrews": "HEB",
+#     "James": "JAS",
+#     "1 Peter": "1PE",
+#     "2 Peter": "2PE",
+#     "1 John": "1JN",
+#     "2 John": "2JN",
+#     "3 John": "3JN",
+#     "Jude": "JUD",
+#     "Revelation": "REV"
+# }
+
+def call_complete_api(translation_id: str):
+    url = f"{BASE_URL}/{translation_id}/complete.json"
+    data = requests.get(url)
+
+    return data.json()
+
+
+if __name__ == "__main__":
+    raw_data = call_complete_api(TRANSLATION_ID)
+
+    print(raw_data)
